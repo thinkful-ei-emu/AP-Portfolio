b20449bf7a1d8a3b1ddcbd318005fe70d9f0a9f0
Arpan Patel <adp8ke@gmail.com>
2019-06-22 21:22:49 -0400

    added in more css and added scroll animation to js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //will need autoscroll fxn and tags and marking
+
+$(".jumper").on("click", function(event) {
+    
+    event.preventDefault();
+
+    $("body, html").animate({ 
+        scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 600);
+    
+});
