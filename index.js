f754ad0535b58874726ad747d88f22ea10ce3fe4
Arpan Patel <adp8ke@gmail.com>
2019-10-09 21:03:47 -0400

    added other capstones

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ $(".jumper").on("click", function(event) {
     }, 600);
     
 });
+
+var slideIndex = [1, 1, 1];
+var slideId = ["rmhSlides", "srSlides", "carSlides"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(2, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
