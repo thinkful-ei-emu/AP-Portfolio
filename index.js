886701f31a31b9c5c72f8c17044cdf0c212eb612
Arpan Patel <adp8ke@gmail.com>
2019-10-11 11:14:53 -0400

    fixed offcenter slideshow and cursors

--- index.js
+++ index.js
@@ -10,23 +10,23 @@ $(".jumper").on("click", function(event) {
     
 });
 
-var slideIndex = [1, 1, 1];
-var slideId = ["rmhSlides", "srSlides", "carSlides"]
+let slideIndex = [1, 1, 1];
+let slideId = ["rmhSlides", "srSlides", "carSlides"];
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(2, 2);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
-}
+};
 
 function showSlides(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
   if (n > x.length) {slideIndex[no] = 1}    
   if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
+  for (let i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
   x[slideIndex[no]-1].style.display = "block";  
-}
+};
